9bdc363bc1febe2a891c262d54ea6f57240ea430
Oneteninfo <wangtyiyi@icloud.com>
2018-11-21 23:57:50 +0800

    ceshi

---
+++ Vue/vue-travel/src/pages/home/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="header">
+        <div class="header-left">返回</div>
+        <div class="header-input">输入城市/景点/游玩主题</div>
+        <div class="header-right">城市</div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+    .header
+        display: flex
+        line-height: .86rem
+        background: #00bcd4
+        color: #f0f0f0
+        .header-left
+            width: .64rem
+            float: left
+        .header-input
+            flex: 1
+            height: .64rem
+            line-height: .64rem
+            margin-left: .2rem
+            margin-top: .12rem
+            background: #f0f0f0
+            color: #fff
+            border-radius: .1rem
+        .header-right
+            width: 1.24rem
+            text-align: center
+            float: right
+</style>
