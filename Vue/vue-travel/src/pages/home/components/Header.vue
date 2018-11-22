720946f6c5a533278ada62228e5a16f11608115e
Oneteninfo <wangtyiyi@icloud.com>
2018-11-22 13:47:22 +0800

    add Header

--- Vue/vue-travel/src/pages/home/components/Header.vue
+++ Vue/vue-travel/src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
     <div class="header">
-        <div class="header-left">返回</div>
-        <div class="header-input">输入城市/景点/游玩主题</div>
-        <div class="header-right">城市</div>
+        <div class="header-left">
+            <span class="iconfont icon-back">&#xe624;</span>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe632;</span>
+            输入城市/景点/游玩主题
+        </div>
+        <div class="header-right">
+            城市
+            <span class="iconfont icon-arrow">&#xe64a;</span>
+        </div>
     </div>
 </template>
 
@@ -13,25 +21,34 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+    @import '~styles/varibles.styl';
     .header
         display: flex
         line-height: .86rem
-        background: #00bcd4
+        background: $bgColor
         color: #f0f0f0
         .header-left
             width: .64rem
             float: left
+            .icon-back
+                display: block
+                text-align: center
+                font-size: .4rem
         .header-input
             flex: 1
             height: .64rem
             line-height: .64rem
             margin-left: .2rem
             margin-top: .12rem
-            background: #f0f0f0
-            color: #fff
+            padding-left: .2rem
+            background: #fff
+            color: #eee
             border-radius: .1rem
         .header-right
             width: 1.24rem
             text-align: center
             float: right
+            .icon-arrow
+                margin-left: -.04rem
+                font-size: .24rem
 </style>
