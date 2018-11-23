6324aae30d08f105bb4ddf4cfcf757e736c82067
Oneteninfo <wangtyiyi@icloud.com>
2018-11-24 01:43:27 +0800

    change

--- Vue/vue-travel/src/main.js
+++ Vue/vue-travel/src/main.js
@@ -3,10 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
 import fastClick from 'fastclick'
+import 'swiper/dist/css/swiper.css'
+
+Vue.use(VueAwesomeSwiper)
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
---
+++ Vue/vue-travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="items of swiperList" :key="items.id">
+                <img class="swiper-img" :src="items.imgUrl" />
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper> 
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {id: '0001', imgUrl: 'http://img1.qunarzz.com/p/tts0/1803/2e/b0552e2a9c46b402.jpg_640x360_77ce0d76.jpg'},
+        {id: '0002', imgUrl: 'http://img1.qunarzz.com/p/tts9/1803/eb/ab21dece1058d502.jpg_640x360_67582315.jpg'}
+      ] 
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #eee
+    .wrapper
+        overflow: hidden
+        width: 100%
+        height: 0
+        padding-bottom: 56.25%
+        .swiper-img
+            width: 100%
+</style>
