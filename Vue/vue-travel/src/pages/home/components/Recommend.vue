4efbb9b8c04bc0b4b46a4a89e3c2902e2ef5979e
Oneteninfo <wangtyiyi@icloud.com>
2018-11-27 22:52:51 +0800

    add recommend

---
+++ Vue/vue-travel/src/pages/home/components/Recommend.vue
@@ -0,0 +1,67 @@
+<template>
+	<div>
+		<div class="title">热销推荐</div>
+		<ul>
+			<li class="item border-bottom" v-for="target of RecommendList" :key="target.id">
+				<img class="item-img" :src="target.imgUrl" />
+				<div class="item-info">
+					<p class="item-title">{{target.title}}</p>
+					<p class="item-desc">{{target.desc}}</p>
+					<button class="item-button">查看详情</button>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'HomeRecommend',
+	data () {
+		return {
+			RecommendList: [
+				{id: '0001', imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_200x200_1bc99086.jpg', title: '故宫', desc: '世界五大宫之首，穿越与您近在咫尺'},
+				{id: '0002', imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_200x200_1bc99086.jpg', title: '故宫', desc: '世界五大宫之首，穿越与您近在咫尺'},
+				{id: '0003', imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_200x200_1bc99086.jpg', title: '故宫', desc: '世界五大宫之首，穿越与您近在咫尺'},
+				{id: '0004', imgUrl: 'http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_200x200_1bc99086.jpg', title: '故宫', desc: '世界五大宫之首，穿越与您近在咫尺'}
+			]
+		}
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
+	.title
+		margin-top .2rem
+		line-height .8rem
+		background-color #eee
+		text-indent .2rem
+	.item
+		overflow hidden
+		display flex
+		height 1.9rem
+		.item-img
+			width 1.7rem
+			height 1.7rem
+			padding .1rem
+		.item-info
+			flex 1
+			padding .1rem
+			min-width 0
+			.item-title
+				line-height .54rem
+				font-size .32rem
+				ellipsis()
+			.item-desc
+				line-height .4rem
+				color #ccc
+				ellipsis()
+			.item-button
+				margin-top .2rem
+				padding 0 .2rem
+				line-height .44rem
+				background-color #ff9300
+				border-radius .06rem
+				color #fff
+</style>
---
+++ Vue/vue-travel/src/pages/home/components/Weekend.vue
@@ -0,0 +1,59 @@
+<template>
+	<div>
+		<div class="title">周末去哪玩儿 </div>
+		<ul>
+			<li class="item border-bottom" v-for="target of RecommendList" :key="target.id">
+				<div class="item-img-wrapper">
+					<img class="item-img" :src="target.imgUrl" />
+				</div>
+				<div class="item-info">
+					<p class="item-title">{{target.title}}</p>
+					<p class="item-desc">{{target.desc}}</p>
+				</div>
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'HomeWeekend',
+	data () {
+		return {
+			RecommendList: [
+				{id: '0001', imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg', title: '京城人的周末撒欢地', desc: '在帝都过周末，不仅仅是城中游！'},
+				{id: '0002', imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg', title: '京城人的周末撒欢地', desc: '在帝都过周末，不仅仅是城中游！'},
+				{id: '0003', imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg', title: '京城人的周末撒欢地', desc: '在帝都过周末，不仅仅是城中游！'},
+				{id: '0004', imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg', title: '京城人的周末撒欢地', desc: '在帝都过周末，不仅仅是城中游！'}
+			]
+		}
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
+	.title
+		margin-top .2rem
+		line-height .8rem
+		background-color #eee
+		text-indent .2rem
+	.item-img-wrapper
+		overflow hidden
+		padding-bottom 33.5%
+		height 0
+		.item-img
+			width 100%
+	.item-info
+		flex 1
+		padding .1rem
+		min-width 0
+		.item-title
+			line-height .54rem
+			font-size .32rem
+			ellipsis()
+		.item-desc
+			line-height .4rem
+			color #ccc
+			ellipsis()
+</style>
