7669eca553a5d20d685c6a684fea2755877b752d
Oneteninfo <wangtyiyi@icloud.com>
2018-12-02 19:46:11 +0800

    add CityList

--- Vue/vue-travel/src/pages/city/City.vue
+++ Vue/vue-travel/src/pages/city/City.vue
@@ -2,17 +2,23 @@
 	<div>
 		<city-header></city-header>
 		<city-search></city-search>
+		<city-list></city-list>
+		<city-alphater></city-alphater>
 	</div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphater from './components/Alphater'
 export default {
 	name: 'City',
 	components: {
 		CityHeader,
-		CitySearch
+		CitySearch,
+		CityList,
+		CityAlphater
 	}
 }
 </script>
---
+++ Vue/vue-travel/src/pages/city/components/List.vue
@@ -0,0 +1,151 @@
+<template>
+	<div class="list" ref="wrapper">
+		<div>
+			<div class="area">
+				<div class="title border-topbottom">当前城市</div>
+				<div class="botton-list">
+					<div class="botton-wrapper">
+						<div class="botton">北京</div>
+					</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="title border-topbottom">热门城市</div>
+				<div class="botton-list">
+					<div class="botton-wrapper">
+						<div class="botton">北京</div>
+					</div>
+					<div class="botton-wrapper">
+						<div class="botton">北京</div>
+					</div>
+					<div class="botton-wrapper">
+						<div class="botton">北京</div>
+					</div>
+					<div class="botton-wrapper">
+						<div class="botton">北京</div>
+					</div>
+					<div class="botton-wrapper">
+						<div class="botton">北京</div>
+					</div>
+					<div class="botton-wrapper">
+						<div class="botton">北京</div>
+					</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="title border-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="title border-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="title border-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="title border-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="title border-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="title border-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+				</div>
+			</div>
+			<div class="area">
+				<div class="title border-topbottom">A</div>
+				<div class="item-list">
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+					<div class="item border-bottom">阿里尔</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import Bscroll from 'better-scroll'
+export default {
+	name: 'CityList',
+	mounted () {
+		this.scroll = new Bscroll(this.$refs.wrapper)
+	}
+}
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
+	.border-topbottom
+		&:before
+			border-color: #ccc
+		&:after
+			border-color: #ccc
+	.border-bottom
+		&:before
+			border-color: #ccc
+	.list
+		overflow: hidden
+		position: absolute
+		top: 1.58rem
+		left: 0
+		right: 0
+		bottom: 0
+		.title
+			line-height: .54rem
+			background-color: #eee
+			padding-left: .2rem
+			color: #666
+			font-size: .26rem
+		.botton-list
+			overflow: hidden
+			padding: .1rem .6rem .1rem .1rem
+			.botton-wrapper
+				float: left
+				width: 33.33%
+				.botton
+					margin: .1rem
+					padding: .1rem 0
+					text-align: center
+					border: .02rem solid #ccc
+					border-radius: .06rem
+		.item-list
+			.item
+				line-height: .54rem
+				padding-left: .2rem
+</style>
