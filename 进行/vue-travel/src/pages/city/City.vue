e48d389cf37ce579ecd98daa5a36bc169a1b8a6b
Oneteninfo <wangtyiyi@icloud.com>
2018-12-05 18:16:27 +0800

    add touchMove

--- 进行/vue-travel/src/pages/city/City.vue
+++ 进行/vue-travel/src/pages/city/City.vue
@@ -2,8 +2,8 @@
 	<div>
 		<city-header></city-header>
 		<city-search></city-search>
-		<city-list :cities="cities" :hot="hotCities"></city-list>
-		<city-alphater :cities="cities"></city-alphater>
+		<city-list :cities="cities" :hot="hotCities" :letter="letter"></city-list>
+		<city-alphater :cities="cities" @change="handleLetterChange"></city-alphater>
 	</div>
 </template>
 
@@ -18,7 +18,8 @@ export default {
 	data () {
 		return {
 			cities: {},
-			hotCities: []
+			hotCities: [],
+			letter: ''
 		}
 	},
 	components: {
@@ -39,6 +40,9 @@ export default {
 				this.cities = data.cities;
 				this.hotCities = data.hotCities;
 			}
+		},
+		handleLetterChange (letter) {
+			this.letter = letter
 		}
 	},
 	mounted () {
